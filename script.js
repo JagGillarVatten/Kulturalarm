732b8eeea794e79fdbfcdbfa712f01c0054fefd2
JagGillarVatten <73820032+JagGillarVatten@users.noreply.github.com>
2023-10-20 10:17:27 +0200

    Gjorde färg systemet mer effektivt

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+/**
+ * Global variables to store event data and duration.
+ * eventFiles contains the schema options to load.
+ * loadJSON loads JSON data from a URL.
+ * getTodaysEvents filters events to only today's.
+ * getNextEvent gets the next upcoming event.
+ * updateCountdown calculates and displays the countdown UI.
+ * formatSeconds and pad format time strings.
+ * loadEventFile loads a schema's JSON data.
+ * init sets up the schema dropdown UI.
+ * parseRSS parses an RSS feed into HTML.
+ */
+
 let events = [];
+
 let eventDuration = 0;
+
 const eventFiles = [
   {
     name: "MP1-23",
@@ -34,177 +49,150 @@ function getNextEvent() {
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   const todaysEvents = getTodaysEvents();
   for (let i = 0; i < todaysEvents.length; i++) {
-    const start = new Date(
-      `${today.toDateString()} ${todaysEvents[i].startTime}`,
-    );
-    const end = new Date(`${today.toDateString()} ${todaysEvents[i].endTime}`);
-    if (now >= start && now < end) {
-      // We're currently in this event
-      return {
-        name: todaysEvents[i].name,
-        start: start,
-        end: end,
-      };
-    } else if (now < start) {
-      // This event hasn't started yet
-      return {
-        name: todaysEvents[i].name,
-        start: start,
-        end: end,
-      };
+      const start = new Date(
+        `${today.toDateString()} ${todaysEvents[i].startTime}`
+      );
+      const end = new Date(`${today.toDateString()} ${todaysEvents[i].endTime}`);
+      if (now >= start && now < end) {
+        // We're currently in this event
+        return {
+          name: todaysEvents[i].name,
+          start: start,
+          end: end,
+        };
+      } else if (now < start) {
+        // This event hasn't started yet
+        return {
+          name: todaysEvents[i].name,
+          start: start,
+          end: end,
+        };
+      }
     }
+    // There are no events left today
+    return null;
   }
 
-  // There are no events left today
-  return null;
-}
+  function updateCountdown() {
+    if (events.length === 0) {
+      // JSON hasn't loaded yet, try again later
+      setTimeout(updateCountdown, 1000);
+      return;
+    }
 
-function updateCountdown() {
-  if (events.length === 0) {
-    // JSON hasn't loaded yet, try again later
-    setTimeout(updateCountdown, 1000);
-    return;
-  }
+    const now = new Date();
+    const nextEvent = getNextEvent();
 
-  const now = new Date();
-  const nextEvent = getNextEvent();
+    if (nextEvent === null) {
+      // No events left today
+      document.getElementById("countdown").innerHTML =
+        "Inga fler lektioner idag.";
+      document.getElementById("progress-bar").style.display = "none";
+      return;
+    }
 
-  if (nextEvent === null) {
-    // No events left today
-    document.getElementById("countdown").innerHTML =
-      "Inga fler lektioner idag.";
-    document.getElementById("progress-bar").style.display = "none";
-    return;
-  }
+    const eventName = nextEvent.name;
+    const start = nextEvent.start;
+    const end = nextEvent.end;
 
-  const eventName = nextEvent.name;
-  const start = nextEvent.start;
-  const end = nextEvent.end;
+    if (now < start) {
+      // Event hasn't started yet
+      const timeUntilStart = (start - now) / 1000; // Convert to seconds
+      const timeUntilStartFormatted = formatSeconds(timeUntilStart);
+      document.title = `${timeUntilStartFormatted} tills | ${eventName}`;
+      const countdown = ` ${eventName} börjar om: <br> ${formatSeconds(
+        timeUntilStart
+      )} `;
+      document.getElementById("countdown").innerHTML = countdown;
+      setCountdownAnimation("Shake", 0.1, 100);
+      setProgressBarColor("#a3d47a");
+      return;
+    }
 
-  if (now < start) {
-    // Event hasn't started yet
-    const timeUntilStart = (start - now) / 1000; // Convert to seconds
-    const timeUntilStartFormatted = formatSeconds(timeUntilStart);
-    document.title = `${timeUntilStartFormatted} tills | ${eventName}`;
-    const countdown = ` ${eventName} börjar om: <br> ${formatSeconds(
-      timeUntilStart,
-    )} `;
+    // Event is ongoing or already finished
+    const timeElapsed = (now - start) / 1000; // Convert to seconds
+    const timeRemaining = (end - now) / 1000; // Convert to seconds
+    const eventDuration = (end - start) / 1000; // convert to seconds
+
+    const countdown = `Tid kvar för ${eventName}:<br> ${formatSeconds(
+      timeRemaining
+    )}`;
     document.getElementById("countdown").innerHTML = countdown;
-    document.getElementById("countdown").style.color = "#ffff";
-    document.getElementById("progress").style.width = "0";
-    document.getElementById("progress").style.backgroundColor = "#a3d47a";
+    const timeRemainingFormatted = formatSeconds(timeRemaining);
+    document.title = `${timeRemainingFormatted} kvar | ${eventName}`;
+    const percentElapsed = (timeElapsed / eventDuration) * 100;
+    const width =
+      (percentElapsed / 100) *
+      document.getElementById("progress-bar").offsetWidth;
+    document.getElementById("progress").style.width = `${width}px`;
+
+    switch (true) {
+      case timeRemaining < 60:
+        setCountdownAnimation("Shake", null, "infinite");
+        setProgressBarColor("#f36868");
+        document.getElementById("countdown").style.color = "#f36868";
+        break;
+      case timeRemaining < 300:
+        setCountdownAnimation("Shake", 0.1, 100);
+        setProgressBarColor("#f7d26f");
+        document.getElementById("countdown").style.color = "#fffff";
+        break;
+      case timeRemaining < 600:
+        setProgressBarColor("#d3ff42");
+        document.getElementById("countdown").style.color = "#fdd5d5";
+        break;
+      default:
+        setProgressBarColor("#a3d47a");
+        document.getElementById("countdown").style.color = "#fdd25";
+        break;
+    }
+
     document.getElementById("progress-bar").style.display = "block";
-    return;
   }
 
-  // Event is ongoing or already finished
-  const totalDuration = (end - start) / 1000; // Convert to seconds
-  const timeElapsed = (now - start) / 1000; // Convert to seconds
-  const timeRemaining = (end - now) / 1000; // Convert to seconds
-  const eventDuration = (end - start) / 1000; // convert to seconds
-
-  if (timeRemaining < 300 && timeRemaining >= 60) {
-    // Change color to yellow when there's less than 5 minutes left
-    document.getElementById("countdown").style.color = "#301c30";
-    document.getElementById("countdown").style.animation = "Shake 0.1s";
-    document.getElementById("countdown").style.animationIterationCount = "100";
-    document.getElementById("progress").style.backgroundColor = "#301c30";
-  } else if (timeRemaining < 60) {
-    // Change color to red when there's less than 1 minute left
-    document.getElementById("countdown").style.color = "#f36868";
-    document.getElementById("progress").style.backgroundColor = "#f36868";
-
-    document.getElementById("countdown").style.animation = "shake";
-    document.getElementById("countdown").style.animationIterationCount =
-      "infinite";
-    document.getElementById("countdown").style.animation.iteration = "100";
-    document.getElementById("progress").style.backgroundColor = "#f26868";
-  } else if (timeRemaining < 600 && timeRemaining >= 300) {
-    // Change color to red when there's less than 1 minute left
-    document.getElementById("countdown").style.color = "#fdd5d5";
-    document.getElementById("progress").style.backgroundColor = "#d3ff42";
-  } else if (timeRemaining > 600) {
-    // Change color to green when there's more than minute left
-    document.getElementById("countdown").style.color = "#fdd25";
-    document.getElementById("progress").style.backgroundColor = "#a3d47a";
+  function setCountdownAnimation(animationName, animationDuration, iterationCount) {
+    const countdown = document.getElementById("countdown");
+    countdown.style.animationName = animationName;
+    countdown.style.animationDuration = animationDuration ? `${animationDuration}s` : null;
+    countdown.style.animationIterationCount = iterationCount;
   }
 
-  const countdown = `Tid kvar för ${eventName}:<br> ${formatSeconds(
-    timeRemaining,
-  )}`;
-  document.getElementById("countdown").innerHTML = countdown;
-  const timeRemainingFormatted = formatSeconds(timeRemaining);
-  document.title = `${timeRemainingFormatted} kvar | ${eventName}`;
-  const percentElapsed = (timeElapsed / eventDuration) * 100;
-  const width =
-    (percentElapsed / 100) *
-    document.getElementById("progress-bar").offsetWidth;
-  document.getElementById("progress").style.width = `${width}px`;
-
-  document.getElementById("progress-bar").style.display = "block";
-}
-
-function formatSeconds(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const paddedSeconds = pad(Math.floor(seconds) % 60, 2);
-  if (hours > 0) {
-    return `${hours}:${pad(minutes % 60, 2)}:${paddedSeconds}`;
-  }
-  return `${pad(minutes, 2)}:${paddedSeconds}`;
-}
-
-function pad(num, size) {
-  return ("000000000" + num).substr(-size);
-}
-
-function loadEventFile(url) {
-  loadJSON(function (response) {
-    events = JSON.parse(response);
-    updateCountdown();
-  }, url);
-}
-
-function init() {
-  const dropdownContent = document.querySelector(".dropdown-content");
-  const dropdownButton = document.querySelector(".dropdown-button");
-  eventFiles.forEach((eventFile) => {
-    const eventButton = document.createElement("a");
-    eventButton.innerText = eventFile.name;
-    eventButton.onclick = () => {
-      loadEventFile(eventFile.url);
-    };
-    dropdownContent.appendChild(eventButton);
-  });
-  loadEventFile(eventFiles[0].url);
-}
-
-setInterval(updateCountdown, 100);
-
-function parseRSS(rss) {
-  const parser = new DOMParser();
-  const xmlDoc = parser.parseFromString(rss, "text/xml");
-
-  const items = xmlDoc.getElementsByTagName("item");
-  let html = "<ul>";
-
-  for (let i = 0; i < items.length; i++) {
-    const title = items[i].getElementsByTagName("title")[0].textContent;
-    const description =
-      items[i].getElementsByTagName("description")[0].textContent;
-
-    html += `<li><strong>${title}</strong>: ${description}</li>`;
+  function setProgressBarColor(color) {
+    document.getElementById("progress").style.backgroundColor = color;
   }
 
-  html += "</ul>";
+  function formatSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const paddedSeconds = pad(Math.floor(seconds) % 60, 2);
+    if (hours > 0) {
+      return `${hours}:${pad(minutes % 60, 2)}:${paddedSeconds}`;
+    }
+    return `${pad(minutes, 2)}:${paddedSeconds}`;
+  }
 
-  const rssContainer = document.createElement("div");
-  rssContainer.innerHTML = html;
+  function pad(num, size) {
+    return ("000000000" + num).substr(-size);
+  }
 
-  const body = document.getElementsByTagName("body")[0];
-  body.insertBefore(rssContainer, body.firstChild);
-}
+  function loadEventFile(url) {
+    loadJSON(function (response) {
+      events = JSON.parse(response);
+      updateCountdown();
+    }, url);
+  }
 
-loadJSON(function (response) {
-  parseRSS(response);
-}, "https://skolmaten.se/hammarbyskolan/rss/days/");
+  function init() {
+    const dropdownContent = document.querySelector(".dropdown-content");
+    eventFiles.forEach((eventFile) => {
+      const eventButton = document.createElement("a");
+      eventButton.innerText = eventFile.name;
+      eventButton.onclick = () => {
+        loadEventFile(eventFile.url);
+      };
+      dropdownContent.appendChild(eventButton);
+    });
+    loadEventFile(eventFiles[0].url);
+  }
+
+  setInterval(updateCountdown, 100);
